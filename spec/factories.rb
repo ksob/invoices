8b6cab9163a72eeb03dea540d07a7f4ac7f07dbf
Kamil Sobieraj <sobieraj.k.2@gmail.com>
2013-11-14 15:58:41 +0100

    refactor specs

---
+++ spec/factories.rb
@@ -0,0 +1,30 @@
+FactoryGirl.define do
+  factory :invoice_item do
+    price 123
+    name "abc"
+    invoice
+  end
+
+  # user factory without associated posts
+  factory :invoice do
+    payment_period "12.11.2013 - 12.12.2013"
+    invoice_number 1234567
+
+    # user_with_posts will create post data after the user has been created
+    factory :ivoice_with_invoice_items do
+      # posts_count is declared as an ignored attribute and available in
+      # attributes on the factory, as well as the callback via the evaluator
+      ignore do
+        invoice_items_count 5
+      end
+
+      # the after(:create) yields two values; the user instance itself and the
+      # evaluator, which stores all values from the factory, including ignored
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the user is associated properly to the post
+      after(:create) do |invoice, evaluator|
+        FactoryGirl.create_list(:invoice_item, evaluator.invoice_items_count, invoice: invoice)
+      end
+    end
+  end
+end
--- spec/views/invoices/edit.html.erb_spec.rb
+++ spec/views/invoices/edit.html.erb_spec.rb
@@ -1,36 +1,5 @@
 require 'spec_helper'
 
-FactoryGirl.define do
-  factory :invoice_item do
-    price 123
-    name "abc"
-    invoice
-  end
-
-  # user factory without associated posts
-  factory :invoice do
-    payment_period "12.11.2013 - 12.12.2013"
-    invoice_number 1234567
-
-    # user_with_posts will create post data after the user has been created
-    factory :ivoice_with_invoice_items do
-      # posts_count is declared as an ignored attribute and available in
-      # attributes on the factory, as well as the callback via the evaluator
-      ignore do
-        invoice_items_count 5
-      end
-
-      # the after(:create) yields two values; the user instance itself and the
-      # evaluator, which stores all values from the factory, including ignored
-      # attributes; `create_list`'s second argument is the number of records
-      # to create and we make sure the user is associated properly to the post
-      after(:create) do |invoice, evaluator|
-        FactoryGirl.create_list(:invoice_item, evaluator.invoice_items_count, invoice: invoice)
-      end
-    end
-  end
-end
-
 describe "invoices/edit" do
   before(:each) do
       assign(:client_names_with_ids, 
@@ -75,17 +44,27 @@ describe "invoices/edit" do
       end
     end
 
-    it "renders correct values for payment_period and invoice_number" do
+    it "renders correct values for payment_period" do
       render
 
       assert_select "input#invoice_payment_period[value=?]", "12.11.2013 - 12.12.2013"
+    end
+
+    it "renders correct values for invoice_number" do
+      render
+
       assert_select "input#invoice_invoice_number[value=?]", "1234567"
     end
 
-    it "renders correct values for invoice_items" do
+    it "renders correct values for invoice_items' name" do
       render
 
       assert_select "input#invoice_invoice_items_attributes_0_name[value=?]", "abc"
+    end
+
+    it "renders correct values for invoice_items' price" do
+      render
+
       assert_select "input#invoice_invoice_items_attributes_0_price[value=?]", "123"
     end
   end
